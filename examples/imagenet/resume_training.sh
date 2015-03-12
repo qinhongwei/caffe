ab91d487fa4a7b2763a64bb3072cf27d302e2c26
cv <qinhongwei>
2015-03-12 17:25:35 +0800

    bvlc_reference_caffenet minor change

--- examples/imagenet/resume_training.sh
+++ examples/imagenet/resume_training.sh
@@ -2,4 +2,7 @@
 
 ./build/tools/caffe train \
     --solver=models/bvlc_reference_caffenet/solver.prototxt \
-    --snapshot=models/bvlc_reference_caffenet/caffenet_train_iter_420000.solverstate
+    #--snapshot=models/bvlc_reference_caffenet/caffenet_train_iter_420000.solverstate
+    --weights=models/bvlc_reference_caffenet/caffenet_train_iter_420000.caffemodel
+
+    #--weights=models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel
